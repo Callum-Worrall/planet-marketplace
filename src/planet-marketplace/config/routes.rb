b9950e2ded0b81e640b5de2ea43de98a53ee1219
Callum Worrall <callum.worrall71@gmail.com>
2020-08-05 11:47:23 +1000

    Removed old references to user.

--- src/planet-marketplace/config/routes.rb
+++ src/planet-marketplace/config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "listings#home"
 
-  get    '/signup'   , to: 'users#signup', as: "user_signup" #new
-  post   '/users'    , to: 'users#create'
+  # get    '/signup'   , to: 'users#signup', as: "user_signup" #new
+  # post   '/users'    , to: 'users#create'
 
-  get    '/login'    , to: 'users#login',     as: "user_login"
+  # get    '/login'    , to: 'users#login',     as: "user_login"
 
-  get    '/user/:id' , to: 'users#edit',      as: "edit_user"
-  patch  '/user/:id' , to: 'users#update',    as: "update_patch_user"
-  put    '/user/:id' , to: 'users#update',    as: "update_put_user"
-  delete '/user/:id' , to: 'users#destroy',   as: "destroy_user"
+  # get    '/user/:id' , to: 'users#edit',      as: "edit_user"
+  # patch  '/user/:id' , to: 'users#update',    as: "update_patch_user"
+  # put    '/user/:id' , to: 'users#update',    as: "update_put_user"
+  # delete '/user/:id' , to: 'users#destroy',   as: "destroy_user"
 
 end
